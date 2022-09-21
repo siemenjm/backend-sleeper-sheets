3a28758d73eb2ba5b5b94fb493be8e65b2199737
Jared Siemen <siemenjm@gmail.com>
2022-09-21 14:19:53 -0400

    Refactor user-controller to get index, create, and show routes working

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const { restart } = require('nodemon');
 const router = express.Router();
+const { People, User } = require('../models');
 
 ///////////////////////////////
 // ROUTES
@@ -7,17 +9,32 @@ const router = express.Router();
 
 // USER INDEX ROUTE
 router.get("/", async (req, res) => {
-	res.status(200).json({message: "user index route"});
+    try {
+        const allUsers = await User.find({});
+        res.json(allUsers);
+    } catch(err) {
+        res.status(400).json(err);
+    }
 });
 
 // USER CREATE ROUTE
 router.post("/", async (req, res) =>  {
-	res.status(201).json({message: "user create route"});
+    try {
+        const createdUser = await User.create(req.body);
+        res.json(createdUser);
+    } catch(err) {
+        res.status(400).json(err);
+    }
 });
 
 // USER SHOW ROUTE
 router.get("/:userId", async (req, res) => {
-	res.status(200).json({message: "user show route: " + req.params.userId});
+	try {
+        const singleUser = await User.findById(req.params.userId);
+        res.json(singleUser);
+    } catch(err) {
+        res.status(400).json(err);
+    }
 });
 
 // USER DELETE ROUTE
