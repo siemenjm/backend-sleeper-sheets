6a6a2f4615cf36f8b067b151c168962e8fc58c03
Jared Siemen <siemenjm@gmail.com>
2022-09-21 14:04:12 -0400

    Add other RESTful routes to users controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -15,4 +15,20 @@ router.post("/", async (req, res) =>  {
 	res.status(201).json({message: "user create route"});
 });
 
+// USER SHOW ROUTE
+router.get("/:userId", async (req, res) => {
+	res.status(200).json({message: "user show route: " + req.params.userId});
+});
+
+// USER DELETE ROUTE
+router.delete("/:userId", async (req, res) => {
+	res.status(200).json({message: "user delete route: " + req.params.userId});
+});
+
+// USER UPDATE ROUTE
+router.put("/:userId", async (req, res) => {
+	console.log(req.body)
+	res.status(200).json({message: "user update route: " + req.params.userId});
+});
+
 module.exports = router;
