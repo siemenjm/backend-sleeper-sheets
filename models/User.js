7871a54a8e2effefb23c47142afc6193326f95f9
Jared Siemen <siemenjm@gmail.com>
2022-09-21 14:09:36 -0400

    Add User model

---
+++ models/User.js
@@ -0,0 +1,27 @@
+///////////////////////////////
+// DEPENDENCIES
+////////////////////////////////
+const mongoose = require("mongoose");
+
+///////////////////////////////
+// MODELS
+////////////////////////////////
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  sleeperName: {
+    type: String,
+    required: true,
+  }
+},{timestamps: true});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
