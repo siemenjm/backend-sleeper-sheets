4daadcebd2b7386918611b087c78340e5573e405
Jared Siemen <siemenjm@gmail.com>
2022-09-22 13:29:32 -0400

    Cleaned up code

--- config/db.connection.js
+++ config/db.connection.js
@@ -2,7 +2,6 @@
 // DEPENDENCIES
 ////////////////////////////////
 
-// pull PORT from .env, give default value of 4000
 const mongoose = require('mongoose');
 const {MONGODB_URI} = process.env;
 
@@ -11,7 +10,6 @@ const {MONGODB_URI} = process.env;
 ////////////////////////////////
 mongoose.connect(MONGODB_URI);
 
-// Connection Events
 mongoose.connection
   .on("open", () => console.log("Your are connected to mongoose"))
   .on("close", () => console.log("Your are disconnected from mongoose"))
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const { restart } = require('nodemon');
 const router = express.Router();
-const { People, User } = require('../models');
+const { User } = require('../models');
 
 ///////////////////////////////
 // ROUTES
